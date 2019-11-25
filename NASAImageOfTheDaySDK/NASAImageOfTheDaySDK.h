3f9031bc52a5ebe7e42719447de82562ab3b544d
Stewart Thomson <stewart.thomson@bemo.co>
2019-11-25 20:37:39 +0000

    Initial Commit

---
+++ NASAImageOfTheDaySDK/NASAImageOfTheDaySDK.h
@@ -0,0 +1,19 @@
+//
+//  NASAImageOfTheDaySDK.h
+//  NASAImageOfTheDaySDK
+//
+//  Created by Stewart Thomson on 25/11/2019.
+//  Copyright © 2019 Stewart Thomson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NASAImageOfTheDaySDK.
+FOUNDATION_EXPORT double NASAImageOfTheDaySDKVersionNumber;
+
+//! Project version string for NASAImageOfTheDaySDK.
+FOUNDATION_EXPORT const unsigned char NASAImageOfTheDaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NASAImageOfTheDaySDK/PublicHeader.h>
+
+
